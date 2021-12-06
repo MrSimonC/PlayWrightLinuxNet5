b771b8eba3ff096cb5221a723681785580c9ec0a
Simon Crouch <iamontheinternet@yahoo.com>
2021-12-06 20:49:08 +0000

    add working playwright c# code

--- PlayWrightLinuxNet5/Function1.cs
+++ PlayWrightLinuxNet5/Function1.cs
@@ -1,24 +1,35 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
 
 namespace PlayWrightLinuxNet5
 {
-    public static class Function1
+    public class Function1
     {
         [Function("Function1")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("Function1");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            var log = executionContext.GetLogger("Function1");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            //response.WriteString("Welcome to Azure Functions!");
+
+            log.LogInformation("Sac starting playwright");
+
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+
+            log.LogInformation("Sac playwright end");
 
             return response;
         }
